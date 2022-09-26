23fa56c0e90c447e8d1d7fb785d3fbe90f66edd4
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-26 12:57:12 +0900

    [추가] 내장 웹서버2 프로젝트 생성

---
+++ embedded_webserver2/src/main/java/kr/yh/EmbeddedWebserver2Application.java
@@ -0,0 +1,13 @@
+package kr.yh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedWebserver2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddedWebserver2Application.class, args);
+    }
+
+}
---
+++ embedded_webserver2/src/test/java/kr/yh/EmbeddedWebserver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.yh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedWebserver2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
