ba79416bd00153dc2c8bb1c1d12bf41a701ecb19
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-09-26 12:50:16 +0900

    [수정] 프로젝트 구조 수정

--- embedded_webserver1/src/main/java/com/yh/basic/EmbeddedWebServerApplication.java
+++ embedded_webserver1/src/main/java/com/yh/basic/EmbeddedWebServerApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicApplication {
+public class EmbeddedWebServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicApplication.class, args);
+		SpringApplication.run(EmbeddedWebServerApplication.class, args);
 
 	}
 }
