0c287a3f2e7a268224b5a41d6b44963c0fc5718c
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-11-06 13:34:01 +0900

    [수정] 코드 수정

---
+++ spring_init/src/test/java/kr/yh/SpringInitApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.yh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springboot_utilization/src/test/java/kr/yh/SpringbootUtilizationApplicationTests.java
@@ -0,0 +1,11 @@
+package kr.yh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUtilizationApplicationTests {
+
+}
